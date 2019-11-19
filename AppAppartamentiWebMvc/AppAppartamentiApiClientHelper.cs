91eed6f0fd50d72326b4ff4181e201d8ba83b4e6
simonebittidev <simone.bitti.dev@gmail.com>
2019-11-19 08:03:53 +0100

    modifiche varie

---
+++ AppAppartamentiWebMvc/AppAppartamentiApiClientHelper.cs
@@ -0,0 +1,207 @@
+﻿using AppAppartamentiApiClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AppAppartamenti.Api
+{
+    public static class ApiHelper
+    {
+        public enum LoginProvider
+        {
+            Facebook = 0,
+            Google = 1,
+            Email = 2
+        }
+
+        public const string AccessTokenKey = "Access_Token";
+        public const string ProviderKey = "Provider_Key";
+
+        public class BearerToken
+        {
+            [JsonProperty("access_token")]
+            public string AccessToken { get; set; }
+
+            [JsonProperty("token_type")]
+            public string TokenType { get; set; }
+
+            [JsonProperty("expires_in")]
+            public string ExpiresIn { get; set; }
+
+            [JsonProperty("userName")]
+            public string UserName { get; set; }
+
+            [JsonProperty(".issued")]
+            public string Issued { get; set; }
+
+            [JsonProperty(".expires")]
+            public string Expires { get; set; }
+        }
+
+        /// <summary>
+        /// Setta il token
+        /// </summary>
+        /// <param name="Username"></param>
+        /// <param name="Password"></param>
+        /// <returns></returns>
+        public static async Task<BearerToken> SetTokenAsync(string ApiEndpoint, string Username, string Password)
+        {
+            BearerToken bearerToken = new BearerToken();
+            using (var httpClient = new HttpClient())
+            {
+                Uri Endpoint = new Uri($"{ApiEndpoint}Token");
+
+                var tokenRequest =
+                    new List<KeyValuePair<string, string>>
+                        {
+                        new KeyValuePair<string, string>("grant_type", "password"),
+                        new KeyValuePair<string, string>("username", Username),
+                        new KeyValuePair<string, string>("password", Password)
+                        };
+
+                HttpContent encodedRequest = new FormUrlEncodedContent(tokenRequest);
+
+                HttpResponseMessage response = await httpClient.PostAsync(Endpoint, encodedRequest);
+
+                if (response != null && response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string jsonResponse = response.Content.ReadAsStringAsync().Result;
+                    bearerToken = JsonConvert.DeserializeObject<BearerToken>(jsonResponse);
+
+                    //Application.Current.Properties[AccessTokenKey] = token.AccessToken;
+                }
+                else
+                {
+                    //Se arrivo qui allora email e password sono sbagliati.
+                    throw new ApplicationException("Password o Email errati.");
+                }
+            }
+
+            return bearerToken;
+        }
+
+        //// Ottiene il Token
+        //public static string GetToken()
+        //{
+        //    string accessToken = null;
+
+        //    if (Application.Current.Properties.ContainsKey(AccessTokenKey))
+        //    {
+        //        accessToken = Application.Current.Properties[AccessTokenKey].ToString();
+
+        //        //TODO: controllare la validità del token.
+        //    }
+
+        //    return accessToken;
+        //}
+
+        ///// <summary>
+        ///// Rimuove il token.
+        ///// </summary>
+        ///// <returns></returns>
+        //public static string DeleteToken()
+        //{
+        //    string accessToken = null;
+
+        //    if (Application.Current.Properties.ContainsKey(AccessTokenKey))
+        //    {
+
+
+        //        Application.Current.Properties.Remove(AccessTokenKey);
+        //    }
+
+        //    return accessToken;
+        //}
+
+        ///// <summary>
+        ///// Salva nella cache se l'utente si è loggato con facebook, google o email.
+        ///// </summary>
+        ///// <param name="Provider"></param>
+        //public static void SetProvider(LoginProvider Provider)
+        //{
+        //    Application.Current.Properties[ProviderKey] = Provider;
+        //}
+
+        ///// <summary>
+        ///// Restituisce se l'utente si è loggato con facebook,google o email.
+        ///// </summary>
+        ///// <returns></returns>
+        //public static LoginProvider GetProvider()
+        //{
+        //    LoginProvider provider = LoginProvider.Email;
+
+        //    if (Application.Current.Properties.ContainsKey(ProviderKey))
+        //    {
+        //        provider = (LoginProvider)Application.Current.Properties[ProviderKey];
+        //    }
+
+        //    return provider;
+        //}
+
+
+
+        ///// <summary>
+        ///// Rimuove il provider.
+        ///// </summary>
+        ///// <returns></returns>
+        //public static string RemoveProvider()
+        //{
+        //    string accessToken = null;
+
+        //    if (Application.Current.Properties.ContainsKey(ProviderKey))
+        //    {
+        //        Application.Current.Properties.Remove(ProviderKey);
+        //    }
+
+        //    return accessToken;
+        //}
+
+        ///// <summary>
+        ///// Restituisce il Client da usare per le chiamate all'api
+        ///// </summary>
+        ///// <returns></returns>
+        //public static HttpClient GetApiClient()
+        //{
+        //    HttpClient httpClient = new HttpClient();
+        //    httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", Api.ApiHelper.GetToken());
+        //    return httpClient;
+        //}
+
+        ///// <summary>
+        ///// Metodo per registrarsi
+        ///// </summary>
+        ///// <param name="Email"></param>
+        ///// <param name="Password"></param>
+        ///// <param name="ConfermaPassword"></param>
+        ///// <returns></returns>
+        //public static async Task RegisterAsync(string Email, string Password, string ConfermaPassword)
+        //{
+        //    using (var httpClient = new HttpClient())
+        //    {
+        //        AccountClient accountClient = new AccountClient(httpClient);
+
+        //        //Creo il modello dei dati per la registrazione
+        //        RegisterUserBindingModel registerBindingModel = new RegisterUserBindingModel()
+        //        {
+        //            Email = Email,
+        //            Password = Password,
+        //            ConfirmPassword = ConfermaPassword
+
+        //        };
+
+        //        try
+        //        {
+        //            await accountClient.RegisterAsync(registerBindingModel);
+        //        }
+        //        catch (ApiException ex)
+        //        {
+        //            throw new Exception("Si è verificato un errore" + ex.StatusCode);
+        //        }
+        //    }
+        //}
+    }
+
+}
---
+++ AppAppartamentiWebMvc/wwwroot/js/home.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $("#navPrincipale").hide();
+
+    HideFiltriAggiuntivi();
+
+    //sul focus mostro i filtri aggiuntivi
+    let txtRicerca = document.getElementById("txtRicerca");
+    txtRicerca.addEventListener("focus", ShowFiltriAggiuntivi);
+    txtRicerca.addEventListener("blur", HideFiltriAggiuntivi);
+
+    let btnAccedi = document.getElementById("btnAccedi");
+    btnAccedi.addEventListener("click", ShowModalLogin);
+});
+
+function ShowFiltriAggiuntivi() {
+    $("#divRicercaAggiuntiva").show();
+}
+
+function HideFiltriAggiuntivi() {
+    $("#divRicercaAggiuntiva").hide();
+}
+
+function ShowModalLogin() {
+    $("#modalLogin").modal("show");
+}
