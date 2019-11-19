e94d52070889cea27fcb322b66bef873fb4605ae
Administrator <Administrator@VM023019C>
2019-11-19 07:07:48 +0000

    aggiunto progetto in core 3

---
+++ AppAppartamentiWebCoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppAppartamentiWebCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
