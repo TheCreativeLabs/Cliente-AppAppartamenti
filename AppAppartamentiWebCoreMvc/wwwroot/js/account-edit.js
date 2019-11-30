339037746a4db10474731776fc4da523509dd58d
simonebittidev <simone.bitti.dev@gmail.com>
2019-11-30 11:39:34 +1000

    inserito file js

---
+++ AppAppartamentiWebCoreMvc/wwwroot/js/account-edit.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("#imageid").change(async function () {
+        const file = document.querySelector('#imageid').files[0];
+        let base64 = await toBase64(file);
+        
+        let imageBytes = base64.slice(base64.indexOf(",")).substring(1, base64.lenght);
+        $("#ImmagineProfilo").val(imageBytes);
+        $("#imgFotoProfilo").attr("src", base64);
+    })
+});
+
