7c7e20698d616422514cf5ab695678936429cda1
simonebittidev <simone.bitti.dev@gmail.com>
2019-12-12 19:32:56 +1000

    Modifiche al fe

--- AppAppartamentiApi/Providers/ApplicationOAuthProvider.cs
+++ AppAppartamentiApi/Providers/ApplicationOAuthProvider.cs
@@ -10,6 +10,7 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
 using AppAppartamentiApi.Models;
+using System.Web.Http.Routing;
 
 namespace AppAppartamentiApi.Providers
 {
@@ -36,6 +37,23 @@ namespace AppAppartamentiApi.Providers
             if (user == null)
             {
                 context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+            else if(user.EmailConfirmed == false)
+            {
+
+                //Reinvio la email
+                var code = await userManager.GenerateEmailConfirmationTokenAsync(user.Id);
+
+                UrlHelper url= new UrlHelper();
+                var callbackUrl =  url.Link("Default", new { Controller = "Api/Account", Action = "ConfirmEmail", UserId = user.Id, Code = code });
+
+                await EmailService.SendAsync(user.Email,
+                   "Confirm your account",
+                   "Please confirm your account by clicking this link: <a href=\""
+                                                   + callbackUrl + "\">link</a>");
+
+                context.SetError("invalid_grant", "The user email is not confirmed.");
                 return;
             }
 
