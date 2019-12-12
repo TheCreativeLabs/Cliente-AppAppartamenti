c601a930605b413949d4436f6b0b316bdb92b050
Administrator <Administrator@VM023019C>
2019-12-12 04:51:14 +0000

    retrieve mail api key from properties

--- AppAppartamentiApi/EmailService/EmailService.cs
+++ AppAppartamentiApi/EmailService/EmailService.cs
@@ -18,7 +18,7 @@ namespace AppAppartamentiApi.Providers
         public static async Task SendAsync(string EmailTo, string Subject, string Body)
         {
             //var apiKey = ConfigurationManager.AppSettings["SendGridApiKey"];
-            var apiKey = ConfigurationManager.AppSettings["SendGridApiKey"];
+            var apiKey = Properties.Settings.Default.SendGridApiKey;
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress("AppAppartamenti@app.com", "AppAppartamenti");
             var subject = Subject;
