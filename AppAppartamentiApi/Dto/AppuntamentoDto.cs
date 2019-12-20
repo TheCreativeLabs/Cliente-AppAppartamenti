9fb28b1b054315ed9f480339f55137e0a9349972
Administrator <Administrator@VM023019C>
2019-12-20 01:16:10 +0000

    agenda: orari disponibili e insert annuncio, fix preferiti

--- AppAppartamentiApi/Dto/AppuntamentoDto.cs
+++ AppAppartamentiApi/Dto/AppuntamentoDto.cs
@@ -10,10 +10,10 @@ namespace AppAppartamentiApi
         public Guid IdAnnuncio { get; set; }
 
         //serve nell'output, nell'input consideriamo il current quindi non deve arrivare da FE
-        public Guid IdRichiedente { get; set; }
+        public Guid? IdRichiedente { get; set; }
 
-        [Required]
-        public Guid IdDestinatario { get; set; }
+        //serve nell'outpur, nell'input va bene passarlo ma se non presente lo ricaviamo dal proprietario dell'annuncio
+        public Guid? IdDestinatario { get; set; }
 
         [Required]
         public DateTime Data { get; set; }
