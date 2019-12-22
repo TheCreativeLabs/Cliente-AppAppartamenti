46b55baa178cbbb706c468ac32f3874429aeb8a8
ferra <ferra@LAPTOP-10LNOLQ0>
2019-12-22 04:28:35 +0100

    pagine di loading per login facebook e google

---
+++ AppAppartamentiApi/Controllers/LoadingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppAppartamentiApi.Controllers
+{
+    public class FacebookLoadingController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Facebook Loading Page";
+
+            return View();
+        }
+    }
+
+    public class GoogleLoadingController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Google Loading Page";
+
+            return View();
+        }
+    }
+}
