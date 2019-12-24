2de0833df7087e02b0d4f0fa9ed05ace36d30599
Administrator <Administrator@VM023019C>
2019-12-24 10:47:45 +0000

    userInfoDtoMapper installationId and OsVersion

--- AppAppartamentiApi/Mapper/UserInfoMapper.cs
+++ AppAppartamentiApi/Mapper/UserInfoMapper.cs
@@ -19,6 +19,8 @@ namespace AppAppartamentiApi.Models
             userInfoDto.IdAspNetUser = userInfo.IdAspNetUser;
             userInfoDto.Email = email;
             userInfoDto.PhotoUrl = userInfo.PhotoUrl;
+            userInfoDto.InstallationId = userInfo.InstallationId;
+            userInfoDto.OsVersion = userInfo.OsVersion;
             return userInfoDto;
         }
     }
