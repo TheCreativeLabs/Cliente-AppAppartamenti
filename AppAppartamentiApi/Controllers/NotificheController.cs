db55f48f1be96b8ee05d757352d2e262ee0fb0e6
Administrator <Administrator@VM023019C>
2019-12-24 01:29:42 +0000

    notifiche controller empty

---
+++ AppAppartamentiApi/Controllers/NotificheController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using AppAppartamentiApi.Models;
+using AppAppartamentiApi.Providers;
+using AppAppartamentiApi.Results;
+using System.Linq;
+using System.Web.Http.Description;
+using System.Data.Entity;
+using System.Web.Http.Results;
+using System.Net;
+
+namespace AppAppartamentiApi.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Notifiche")]
+    public class NotificheController : ApiController
+    {
+        private const string LocalLoginProvider = "Local";
+        private ApplicationUserManager _userManager;
+
+        private DbDataContext dbDataContext = new DbDataContext();
+
+
+        public NotificheController()
+        {
+        }
+
+        public NotificheController(ApplicationUserManager userManager,
+            ISecureDataFormat<AuthenticationTicket> accessTokenFormat)
+        {
+            UserManager = userManager;
+            AccessTokenFormat = accessTokenFormat;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public ISecureDataFormat<AuthenticationTicket> AccessTokenFormat { get; private set; }
+
+    }
+}
