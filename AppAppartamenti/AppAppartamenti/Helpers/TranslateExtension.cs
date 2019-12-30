9aa9f68934c627c9792ebfe4fdc06515434fda12
ferra <ferra@LAPTOP-10LNOLQ0>
2019-12-30 01:26:45 +0100

    pre merge

--- AppAppartamenti/AppAppartamenti/Helpers/TranslateExtension.cs
+++ AppAppartamenti/AppAppartamenti/Helpers/TranslateExtension.cs
@@ -36,13 +36,13 @@ namespace AppAppartamenti.Helpers
 
             if (translation == null)
             {
-                #if DEBUG
+#if DEBUG
                 throw new ArgumentException(
                     String.Format("Key '{0}' was not found in resources '{1}' for culture '{2}'.", Text, ResourceId, ci.Name),
                     "Text");
-                #else
+#else
                 translation = Text; // returns the key, which GETS DISPLAYED TO THE USER
-                #endif
+#endif
             }
             return translation;
         }
